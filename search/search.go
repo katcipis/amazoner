d3fe961cf547333819aa5058ca31e245e9b696f0
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-11-27 17:48:47 +0100

    Improve filtering of URLs

--- search/search.go
+++ search/search.go
@@ -1,10 +1,13 @@
 package search
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
@@ -42,31 +45,108 @@ func Do(name string, minPrice uint, maxPrice uint) ([]Result, error) {
 		return nil, fmt.Errorf("main search query failed, unexpected status code %d, body:\n%s\n", res.StatusCode, resBody)
 	}
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+	urls, err := parseResultsURLs(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var errs []error
+	var results []Result
+
+	for _, url := range urls {
+		result, err := getResult(url)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+		results = append(results, result)
+	}
+
+	return results, toErr(errs)
+}
+
+func getResult(url string) (Result, error) {
+	return Result{
+		URL: url,
+	}, nil
+}
+
+func parseResultsURLs(html io.Reader) ([]string, error) {
+	doc, err := goquery.NewDocumentFromReader(html)
 	if err != nil {
 		return nil, err
 	}
 
 	s := doc.Find(".s-main-slot.s-result-list.s-search-results.sg-row")
 	s = s.Find("a")
-	results := []Result{}
+	urls := []string{}
 
 	s.Each(func(i int, s *goquery.Selection) {
 		for _, n := range s.Nodes {
 			attrs := n.Attr
 			for _, attr := range attrs {
-				// WHY: Because I can't find a fucking easier way to get the
-				// fucking links (fuck CSS selectors and fuck HTML).
 				if attr.Key == "href" {
-					results = append(results, Result{URL: attr.Val})
+					urls = append(urls, attr.Val)
 				}
 			}
 		}
 	})
 
-	return results, nil
+	urls = removeStartingWith(urls, "/s/", "/s?", "/gp/")
+	urls = removeDuplicates(urls)
+
+	return urls, nil
+}
+
+func removeStartingWith(urls []string, prefixes ...string) []string {
+	res := []string{}
+
+	for _, url := range urls {
+		hasPrefix := false
+		for _, prefix := range prefixes {
+			if strings.HasPrefix(url, prefix) {
+				hasPrefix = true
+				break
+			}
+		}
+		if !hasPrefix {
+			res = append(res, url)
+		}
+	}
+
+	return res
+}
+
+func removeDuplicates(urls []string) []string {
+	uniq := map[string]struct{}{}
+
+	for _, url := range urls {
+		uniq[url] = struct{}{}
+	}
+
+	i := 0
+	res := make([]string, len(uniq))
+
+	for url, _ := range uniq {
+		res[i] = url
+		i++
+	}
+
+	return res
 }
 
 func itoa(v uint) string {
 	return strconv.FormatUint(uint64(v), 10)
+}
+
+func toErr(errs []error) error {
+	if len(errs) == 0 {
+		return nil
+	}
+
+	errmsgs := make([]string, len(errs))
+	for i, err := range errs {
+		errmsgs[i] = err.Error()
+	}
+	return errors.New(strings.Join(errmsgs, "\n"))
 }
