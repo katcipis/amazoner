0889fa07ec7d6dee48d1894a0161b9a6ceb2070b
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-11-30 13:26:38 +0100

    Extract logic related to product to its package

--- product/product.go
+++ product/product.go
@@ -6,11 +6,11 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/katcipis/amazoner/parser"
 )
 
 type Product struct {
@@ -43,6 +43,30 @@ func Get(url string) (Product, error) {
 	return parseProduct(res.Body)
 }
 
+func ParsePrice(doc *goquery.Document) (float64, bool) {
+	// FIXME: probably just exposing Get or a Parse would be better
+	// instead of these very specific parsing functions.
+
+	if price, ok := parseMoney(doc.Find("#price_inside_buybox").Text()); ok {
+		return price, true
+	}
+
+	if price, ok := parseMoney(doc.Find("#priceblock_ourprice").Text()); ok {
+		return price, true
+	}
+
+	if price, ok := parseMoney(doc.Find("#style_name_0_price").Text()); ok {
+		return price, true
+	}
+
+	if price, ok := parseMoney(doc.Find("#olp-upd-new > span > a > span.a-size-base.a-color-price").Text()); ok {
+		return price, true
+	}
+
+	// Handling more price parsing options will give us more product options
+	return 0, false
+}
+
 func addUserAgent(req *http.Request) {
 	req.Header.Add("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36")
 }
@@ -59,7 +83,7 @@ func parseProduct(html io.Reader) (Product, error) {
 		return Product{}, errors.New("cant parse product name")
 	}
 
-	price, ok := parser.ParseProductPrice(doc)
+	price, ok := ParsePrice(doc)
 	if !ok {
 		return Product{}, errors.New("cant parse product price")
 	}
@@ -68,4 +92,23 @@ func parseProduct(html io.Reader) (Product, error) {
 		Name:  name,
 		Price: price,
 	}, nil
+}
+
+func parseMoney(s string) (float64, bool) {
+	// Yeah using float for money is not great...
+	s = strings.TrimSpace(s)
+	v, err := strconv.ParseFloat(s, 64)
+	if err == nil {
+		return v, true
+	}
+	sp := strings.Split(s, "$")
+	if len(sp) <= 1 {
+		return 0, false
+	}
+
+	v, err = strconv.ParseFloat(sp[1], 64)
+	if err != nil {
+		return 0, false
+	}
+	return v, true
 }
